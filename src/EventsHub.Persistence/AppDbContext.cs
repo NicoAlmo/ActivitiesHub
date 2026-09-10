8396dc1b79baad6b3f166ed9ad03bb26cc841b15
NicoAlmo <nicoalmo.code@gmail.com>
2026-09-09 18:46:17 -0600

    Parcial01-Add API skeleton (#1)
    * feat(parcial01) - add inital boilerplate
    
    * feat(parcial01) - cleanup boilerplate
    
    * feat(parcial01) - add entity and EF boilerplate
    
    * feat(parcial01) - add first entity and inital migration
    
    * feat(parcial01) - add db initializer
    
    * feat(parcial01) - rename slnx, csproj and folders
    
    * feat(parcial01) - add events controller
    
    * feat(parcial01) - add  bruno integration tests
    
    * feat(parcial01) - update bruno integration tests
    
    * feat(parcial01) - add openapi including rpc client generation

---
+++ src/EventsHub.Persistence/AppDbContext.cs
@@ -0,0 +1,8 @@
+using EventsHub.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventsHub.Persistence;
+ public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Event> Events {get;set;}
+}
